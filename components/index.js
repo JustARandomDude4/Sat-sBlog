1bfd08b7943245023ba5838318de1899acb7820c
Hima Satwika Katragadda <83648082+JustARandomDude4@users.noreply.github.com>
2022-03-20 23:05:54 +0530

    added files

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export {default as PostCard} from './PostCard';
+export {default as Categories} from './Categories';
+export {default as PostWidget} from './PostWidget';
+
+
+
+// this is to make exporting the components easy
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
-import Head from 'next/head';
+import { PostCard, Categories, PostWidget } from '../components';
+//import { getPosts } from '../services';
 
-export default function Home() {
+const posts=[
+  {title: 'post1', excerpt: 'posting '},
+  {title:'post2', excerpt:'postingg'}
+];
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-
-      <Head>
-        <title>Sat's Blog</title>
-        <link rel="icon" href="" />
-
-      </Head>
-
-
-
+      
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+/*
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
+*/
